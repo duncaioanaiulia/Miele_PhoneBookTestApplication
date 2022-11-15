8b103387725840297f613b7e10aeda1937b5da08
Ioana Dunca <dunca.ioana.iulia@gmail.com>
2022-11-15 16:52:37 +0200

    move project

---
+++ UserInterface/UserInterface/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UserInterface.ViewModels;
+using UserInterface.Views;
+using Xamarin.Forms;
+
+namespace UserInterface
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ UserInterface/UserInterface/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using UserInterface.ViewModels;
+using Xamarin.Forms;
+
+namespace UserInterface.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
